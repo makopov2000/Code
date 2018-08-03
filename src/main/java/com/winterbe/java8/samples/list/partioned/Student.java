32e41581c1efa6b0bcb10847cf841fb81100d48c
Akopov <makopov@amgen.com>
2018-08-03 13:30:42 -0700

    Added 1. Comparator/CompareToBuilder 2. Collection Split LIST to PArtiones

---
+++ src/main/java/com/winterbe/java8/samples/list/partioned/Student.java
@@ -0,0 +1,36 @@
+package com.winterbe.java8.samples.list.partioned;
+
+public class Student {
+	String name;
+	String course;
+	Integer age;
+	public Student(String name, String course, Integer age) {
+		this.name = name;
+		this.course = course;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", course=" + course + ", age=" + age + "]";
+	}
+	
+	
+}
